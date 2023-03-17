c91c93d7d4c0696de57dc0931c257d7fc46690f3
Jeremy Martin <jeremymartin4794@gmail.com>
2023-03-17 15:53:49 -0400

    fixing routes and converting to flask

--- app.py
+++ app.py
@@ -7,7 +7,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(app)
 
 
+# db tables
+
+
 # routes
 @app.route('/', methods=["GET"])
 def index():
     return render_template("index.html")
+
+@app.route('/login', methods=["GET"])
+def login():
+    return render_template("login.html")
+
+@app.route('/login_pass', methods=["GET"])
+def login_pass():
+    return render_template("login-pass.html")
+
+@app.route('/survey', methods=["GET"])
+def survey():
+    return render_template("mySurvey.html")
+
+@app.route('/success', methods=["GET"])
+def success():
+    return render_template("success.html")
