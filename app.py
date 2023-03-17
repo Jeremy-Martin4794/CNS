b09428604b67a5d8af4d12bf7147ae3c9a5149fd
Jeremy Martin <jeremymartin4794@gmail.com>
2023-03-17 17:00:28 -0400

    adding delays

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ db = SQLAlchemy(app)
 def index():
     return render_template("index.html")
 
+@app.route('/response_index', methods=["GET"])
+def response_index():
+    return render_template("response-index.html")
+
 @app.route('/login', methods=["GET"])
 def login():
     return render_template("login.html")
@@ -23,9 +27,17 @@ def login():
 def login_pass():
     return render_template("login-pass.html")
 
+@app.route('/response_login', methods=["GET"])
+def response_login():
+    return render_template("response-login.html")
+
 @app.route('/survey', methods=["GET"])
 def survey():
     return render_template("mySurvey.html")
+
+@app.route('/response_survey', methods=["GET"])
+def response_survey():
+    return render_template("response-survey.html")
 
 @app.route('/success', methods=["GET"])
 def success():
